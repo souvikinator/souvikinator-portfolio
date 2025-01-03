fc2abe14d5deea35ee9fb2dba0a96dd07a02746a
Emile Choghi <emilechoghi@protonmail.com>
2025-01-03 11:43:50 -0600

    feat: tag counts

--- src/components/ui/mobile-menu.tsx
+++ src/components/ui/mobile-menu.tsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react'
 import { Button } from '@/components/ui/button'
 import { NAV_LINKS } from '@/consts'
-import { Briefcase, Home, Newspaper, Tags } from 'lucide-react'
+import { Archive, Briefcase, Home, Tags } from 'lucide-react'
 import { cn } from '@/lib/utils'
 
 const iconMap = {
   home: () => <Home className="h-5 w-5" />,
-  blog: () => <Newspaper className="h-5 w-5" />,
+  blog: () => <Archive className="h-5 w-5" />,
   tags: () => <Tags className="h-5 w-5" />,
   work: () => <Briefcase className="h-5 w-5" />,
 } as Record<string, () => JSX.Element>
