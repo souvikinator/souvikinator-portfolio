667a83218dfbbc9077628530a54da585b8b9cafb
Emile Choghi <emilechoghi@protonmail.com>
2025-01-02 23:49:01 -0600

    feat: simple mobile menu

--- src/components/ui/mobile-menu.tsx
+++ src/components/ui/mobile-menu.tsx
@@ -1,16 +1,20 @@
 import { useState, useEffect } from 'react'
 import { Button } from '@/components/ui/button'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
 import { NAV_LINKS } from '@/consts'
-import { Menu } from 'lucide-react'
+import { List, Menu, Newspaper, Tags, X } from 'lucide-react'
+
+const iconMap = {
+  blog: () => <Newspaper className="h-6 w-6" />,
+  tags: () => <Tags className="h-6 w-6" />,
+  work: () => <List className="h-6 w-6" />,
+} as Record<string, () => JSX.Element>
 
 const MobileMenu = () => {
   const [isOpen, setIsOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen)
+  }
 
   useEffect(() => {
     const handleViewTransitionStart = () => {
@@ -18,7 +22,6 @@ const MobileMenu = () => {
     }
 
     document.addEventListener('astro:before-swap', handleViewTransitionStart)
-
     return () => {
       document.removeEventListener(
         'astro:before-swap',
@@ -28,32 +31,33 @@ const MobileMenu = () => {
   }, [])
 
   return (
-    <DropdownMenu open={isOpen} onOpenChange={setIsOpen} modal={false}>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="outline"
-          size="icon"
-          className="md:hidden"
-          title="Menu"
-        >
-          <Menu className="h-5 w-5" />
-          <span className="sr-only">Toggle menu</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="bg-background">
-        {NAV_LINKS.map((item) => (
-          <DropdownMenuItem key={item.href} asChild>
-            <a
-              href={item.href}
-              className="w-full text-lg font-medium capitalize"
-              onClick={() => setIsOpen(false)}
-            >
-              {item.label}
-            </a>
-          </DropdownMenuItem>
-        ))}
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div className="flex flex-col items-center lg:hidden">
+      <Button
+        onClick={toggleMenu}
+        className="z-50 bg-background p-2 text-primary focus:outline-none"
+        aria-label="Toggle Menu"
+      >
+        {isOpen ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
+      </Button>
+
+      {isOpen && (
+        <div className="fixed left-0 top-0 z-10 w-full bg-background pb-10 pt-[68px]">
+          <div className="space-y-4 p-4">
+            {NAV_LINKS.map((item) => (
+              <a
+                key={item.href}
+                href={item.href}
+                className="text-md flex w-full items-center justify-start space-x-4 px-4 text-center font-medium capitalize"
+                onClick={() => setIsOpen(false)}
+              >
+                {iconMap[item.label]()}
+                <span>{item.label}</span>
+              </a>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
   )
 }
 
